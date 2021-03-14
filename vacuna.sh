516626c3298256f1223d55022a64dda69b6dca57
joaquin <jdevicente@genasys.com>
2021-03-14 06:16:03 +0100

    Fix

--- vacuna.sh
+++ vacuna.sh
@@ -1,9 +1,12 @@
-
-`sudo wget -O /home/joaquin/vacuna.html https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/situacionActual.htm`
-`sudo grep "<p class=\"cifra\">*" /home/joaquin/vacuna.html | cut -d ">" -f 2 | cut -d "<" -f 1 | tail -n 3 | sed "s/\.//g" > /home/joaquin/vacuna.txt`
-aux1=`sudo head -n 1 /home/joaquin/vacuna.txt`
-aux2=`sudo sed -n '2p' /home/joaquin/vacuna.txt`
-aux3=`sudo tail -n 1 /home/joaquin/vacuna.txt`
+#!/bin/bash
+Fuente: https://www.enmimaquinafunciona.com/pregunta/23759/cual-es-la-diferencia-entre-binsh-binbash
+`sudo wget -O /root/vacuna.html https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/situacionActual.htm`
+`sudo chmod 777 /root/vacuna.html`
+`sudo grep "<p class=\"cifra\">*" /root/vacuna.html | cut -d ">" -f 2 | cut -d "<" -f 1 | tail -n 3 | sed "s/\.//g" > /root/vacuna.txt`
+`sudo chmod 777 /root/vacuna.txt`
+aux1=`sudo head -n 1 /root/vacuna.txt`
+aux2=`sudo sed -n '2p' /root/vacuna.txt`
+aux3=`sudo tail -n 1 /root/vacuna.txt`
 data1=`sudo numfmt --grouping $aux1`
 data2=`sudo numfmt --grouping $aux2`
 data3=`sudo numfmt --grouping $aux3`
@@ -11,11 +14,11 @@ data4=47351567
 template1="Dosis distribuidas:"
 template2="Dosis administradas:"
 template3="Dosis pendientes:"
-template4="Tasa de administracion:"
+template4="Tasa de administración:"
 template5="Parcialmente inmunizados:"
 template6="Totalmente inmunizados:"
-template7="Tasa de inmunizacion parcial:"
-template8="Tasa de inmunizacion completa:"
+template7="Tasa de inmunización parcial:"
+template8="Tasa de inmunización completa:"
 data=`sudo bc -l <<< $aux1-$aux2`
 d1=`sudo numfmt --grouping $data`
 d2=`sudo bc -l <<< $aux2/$aux1*100 | head -c 5`
@@ -24,9 +27,9 @@ d3=`sudo numfmt --grouping $dataaux`
 d4=`sudo bc -l <<< $dataaux/$data4*100 | head -c 5`
 d5=`sudo bc -l <<< $aux3/$data4*100 | head -c 5`
 url="Fuente: Ministerio de Sanidad.\nhttps://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/situacionActual.htm"
-tweet="${template1} ${data1}\n${template2} ${data2}\n${template3} ${d1}\n${template4} ${d2}%\n${template5} ${d3}\n${template6} ${data3}\n${template7} ${d4}%\n${template8} ${d5}%"
-`sudo echo -e "$tweet" > /home/joaquin/vacuna.txt`
-send=`sudo cat /home/joaquin/vacuna.txt`
+tweet="${template1} ${data1}\n${template2} ${data2}\n${template3} ${d1}\n${template4} ${d2}%\n${template5} ${d3}\n${template6} ${data3}\n${template7} ${d4}%\n${template8} ${d5}%..."
+`sudo echo -e "$tweet" > /root/vacuna.txt`
+send=`sudo cat /root/vacuna.txt`
 t update "$send"
-`sudo rm /home/joaquin/vacuna.txt`
-`sudo rm /home/joaquin/vacuna.html`
+`sudo rm /root/vacuna.txt`
+`sudo rm /root/vacuna.html`
