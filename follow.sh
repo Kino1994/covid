d94f1fc6744097b3256836043c326c2bb1341902
joaquin <jdevicente@genasys.com>
2021-03-16 02:18:20 +0100

    Updadted following

--- follow.sh
+++ follow.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-`sudo sleep 600`
-for i in {1..100}
+`sudo sleep 120`
+for i in {1..15}
 do
   u=`sudo sed -n ${i}p followers.txt`
-  t follow "$u"
-  `sudo sleep 10`
+  t follow "$u"`
 done
 a=`sudo wc followers.txt | cut -d ' ' -f 2 | sed -e "s/ //g"`
-b=`sudo bc -l <<< $[a]-300`
+b=`sudo bc -l <<< $[a]-15`
 `sudo head -n "$b" /root/followers.txt >> /root/followers2.txt`
 `sudo rm /root/followers.txt`
 `sudo mv /root/followers2.txt /root/followers.txt`
