0107a7aaa83b0952150081abf48eaef204795340
joaquin <jdevicente@genasys.com>
2021-03-27 10:46:49 +0100

    Fix

--- vacuna.sh
+++ vacuna.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 `sudo sleep 120`
 `sudo wget -O /root/vacuna.html https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/situacionActual.htm`
@@ -21,11 +22,11 @@ template7="Tasa de inmunización parcial:"
 template8="Tasa de inmunización completa:"
 data=`sudo bc -l <<< $aux1-$aux2`
 d1=`sudo numfmt --grouping $data`
-d2=`sudo bc -l <<< $aux2/$aux1*100 | head -c 5`
+d2=`sudo bc -l <<< $aux2/$aux1*100 | head -c 5 | sed -e 's/\./,/g'`
 dataaux=$[$[aux2] - $[aux3]]
 d3=`sudo numfmt --grouping $dataaux`
-d4=`sudo bc -l <<< $dataaux/$data4*100 | head -c 5`
-d5=`sudo bc -l <<< $aux3/$data4*100 | head -c 5`
+d4=`sudo bc -l <<< $dataaux/$data4*100 | head -c 5 | sed -e 's/\./,/g'`
+d5=`sudo bc -l <<< $aux3/$data4*100 | head -c 5 | sed -e 's/\./,/g'`
 url="Fuente: Ministerio de Sanidad.\nhttps://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/situacionActual.htm"
 tweet="${template1} ${data1}\n${template2} ${data2}\n${template3} ${d1}\n${template4} ${d2}%\n${template5} ${d3}\n${template6} ${data3}\n${template7} ${d4}%\n${template8} ${d5}%"
 `sudo echo -e "$tweet" > /root/vacuna.txt`
